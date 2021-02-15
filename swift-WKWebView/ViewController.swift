ab2051bac3ba4776157c34fd446b6d21ed15d537
15951595 <shun19871001@gmail.com>
2021-02-15 22:30:54 +0900

    delegate add

--- swift-WKWebView/ViewController.swift
+++ swift-WKWebView/ViewController.swift
@@ -20,16 +20,31 @@ class ViewController: UIViewController,WKNavigationDelegate {
         webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height)
         
         view.addSubview(webView)
+        webView.navigationDelegate = self
+        
         
         let url =  URL(string: "https://www.kurashiru.com/")
         let request = URLRequest(url: url!)
         webView.load(request)
+        indicator.layer.zPosition = 2
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.isHidden = true
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
     }
 
     @IBAction func back(_ sender: Any) {
+        webView.goBack()
     }
     
     @IBAction func go(_ sender: Any) {
+        webView.goForward()
     }
 }
 
